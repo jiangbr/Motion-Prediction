8db8bed3aeefe622c31589077d22225c55b7598f
jiangbr <brjiang@126.com>
2020-10-18 17:20:04 +0800

    small modificaition

--- agent_motion_prediction.py
+++ agent_motion_prediction.py
@@ -24,7 +24,6 @@ from l5kit.evaluation.chop_dataset import MIN_FUTURE_STEPS
 from l5kit.evaluation.metrics import neg_multi_log_likelihood, time_displace
 from l5kit.geometry import transform_points
 from l5kit.visualization import PREDICTED_POINTS_COLOR, TARGET_POINTS_COLOR, draw_trajectory
-from prettytable import PrettyTable
 from pathlib import Path
 
 import os
@@ -56,42 +55,42 @@ resnet_pre = '../input/resnet_pretrain_dict.pth'
 # In[3]:
 
 
-def build_model(cfg: Dict) -> torch.nn.Module:
+def build_model(cfg_: Dict) -> torch.nn.Module:
     # load pre-trained Conv2D model
-    model = resnet50(pretrained=False)
+    model_ = resnet50(pretrained=False)
 
     # change input channels number to match the rasterizer's output
-    num_history_channels = (cfg["model_params"]["history_num_frames"] + 1) * 2
+    num_history_channels = (cfg_["model_params"]["history_num_frames"] + 1) * 2
     num_in_channels = 3 + num_history_channels
-    model.conv1 = nn.Conv2d(
+    model_.conv1 = nn.Conv2d(
         num_in_channels,
-        model.conv1.out_channels,
-        kernel_size=model.conv1.kernel_size,
-        stride=model.conv1.stride,
-        padding=model.conv1.padding,
+        model_.conv1.out_channels,
+        kernel_size=model_.conv1.kernel_size,
+        stride=model_.conv1.stride,
+        padding=model_.conv1.padding,
         bias=False,
     )
     # change output size to (X, Y) * number of future states
-    num_targets = 2 * cfg["model_params"]["future_num_frames"]
-    model.fc = nn.Linear(in_features=2048, out_features=num_targets)
+    num_targets = 2 * cfg_["model_params"]["future_num_frames"]
+    model_.fc = nn.Linear(in_features=2048, out_features=num_targets)
 
-    return model
+    return model_
 
 
 # In[4]:
 
 
-def forward(data, model, device, criterion):
-    inputs = data["image"].to(device)
-    target_availabilities = data["target_availabilities"].unsqueeze(-1).to(device)
-    targets = data["target_positions"].to(device)
+def forward(data_, model_, device_, criterion_):
+    inputs = data_["image"].to(device_)
+    target_availabilities = data_["target_availabilities"].unsqueeze(-1).to(device_)
+    targets = data_["target_positions"].to(device_)
     # Forward pass
-    outputs = model(inputs).reshape(targets.shape)
-    loss = criterion(outputs, targets)
+    outputs = model_(inputs).reshape(targets.shape)
+    loss_ = criterion_(outputs, targets)
     # not all the output steps are valid, but we can filter them out from the loss using availabilities
-    loss = loss * target_availabilities
-    loss = loss.mean()
-    return loss, outputs
+    loss_ = loss_ * target_availabilities
+    loss_ = loss_.mean()
+    return loss_, outputs
 
 
 # ## Load the Train Data
