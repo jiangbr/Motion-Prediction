b44050ae4a01a80e1c23db70cba149076a951765
jiangbr <brjiang@126.com>
2020-10-26 20:52:41 +0800

    Modify motion_prediction

--- agent_motion_prediction.py
+++ agent_motion_prediction.py
@@ -1,60 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+import os
+import argparse
 from typing import Dict
 
-from tempfile import gettempdir
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision.models.resnet import resnet50
 from tqdm import tqdm
+import torch.multiprocessing as mp
+import torch.distributed as dist
+import torchvision
+import torchvision.transforms as transforms
 
 from l5kit.configs import load_config_data
 from l5kit.data import LocalDataManager, ChunkedDataset
-from l5kit.dataset import AgentDataset, EgoDataset
+from l5kit.dataset import AgentDataset
 from l5kit.rasterization import build_rasterizer
-from l5kit.evaluation import write_pred_csv, compute_metrics_csv, read_gt_csv, create_chopped_dataset
-from l5kit.evaluation.chop_dataset import MIN_FUTURE_STEPS
-from l5kit.evaluation.metrics import neg_multi_log_likelihood, time_displace
-from l5kit.geometry import transform_points
-from l5kit.visualization import PREDICTED_POINTS_COLOR, TARGET_POINTS_COLOR, draw_trajectory
-from pathlib import Path
+from l5kit.evaluation import write_pred_csv
 
-import os
-
-print("成功打开")
-# ## Prepare Data path and load cfg
-# 
-# By setting the `L5KIT_DATA_FOLDER` variable, we can point the script to the folder where the data lies.
-# 
-# Then, we load our config file with relative paths and other configurations (rasteriser, training params...).
-
-# In[2]:
-
+from apex.parallel import DistributedDataParallel as DDP
+from apex import amp
 
 # set env variable for data
 os.environ["L5KIT_DATA_FOLDER"] = "../input/lyft-motion-prediction-autonomous-vehicles"
 dm = LocalDataManager(None)
 # get config
 cfg = load_config_data("./agent_motion_config.yaml")
-print(cfg)
-print(type(dm))
-# log_dir = '../input/resnet_example.pth'
-log_dir = '../input/resnet_pre_test.pth'
-resnet_pre = '../input/resnet_pretrain_dict.pth'
-# ## Model
-# 
-# Our baseline is a simple `resnet50` pretrained on `imagenet`. We must replace the input and the final layer to address our requirements.
 
-# In[3]:
+# model path and saving path
+log_dir = '../input/'
 
 
+# Our baseline is a simple `resnet50` pretrained on `imagenet`.
 def build_model(cfg_: Dict) -> torch.nn.Module:
     # load pre-trained Conv2D model
     model_ = resnet50(pretrained=False)
@@ -77,13 +58,10 @@ def build_model(cfg_: Dict) -> torch.nn.Module:
     return model_
 
 
-# In[4]:
-
-
 def forward(data_, model_, device_, criterion_):
-    inputs = data_["image"].to(device_)
-    target_availabilities = data_["target_availabilities"].unsqueeze(-1).to(device_)
-    targets = data_["target_positions"].to(device_)
+    inputs = data_["image"].cuda(device_)
+    target_availabilities = data_["target_availabilities"].unsqueeze(-1).cuda(device_)
+    targets = data_["target_positions"].cuda(device_)
     # Forward pass
     outputs = model_(inputs).reshape(targets.shape)
     loss_ = criterion_(outputs, targets)
@@ -93,239 +71,146 @@ def forward(data_, model_, device_, criterion_):
     return loss_, outputs
 
 
-# ## Load the Train Data
-# 
-# Our data pipeline map a raw `.zarr` folder into a multi-processing instance ready for training by:
-# - loading the `zarr` into a `ChunkedDataset` object. This object has a reference to the different arrays into the zarr (e.g. agents and traffic lights);
-# - wrapping the `ChunkedDataset` into an `AgentDataset`, which inherits from torch `Dataset` class;
-# - passing the `AgentDataset` into a torch `DataLoader`
+# Training
+def train(gpu, args):
+    print('Begin training with gpu{}'.format(gpu))
+    # distributed training initialization
+    dist.init_process_group(
+        backend='nccl',
+        init_method='tcp://127.0.0.1:12121',
+        world_size=args.world_size,
+        rank=gpu
+    )
+    torch.manual_seed(0)
 
-# In[5]:
+    # INIT MODEL
+    model = build_model(cfg)
+    torch.cuda.set_device(gpu)
+    model.cuda(gpu)
+    print("Finish loading model")
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    criterion = nn.MSELoss(reduction="none")
+
+    # Load the Train Data
+    train_cfg = cfg["train_data_loader"]
+    rasterizer = build_rasterizer(cfg, dm)
+    train_zarr = ChunkedDataset(dm.require(train_cfg["key"])).open()
+    train_dataset = AgentDataset(cfg, train_zarr, rasterizer)
+    print('Finish loading dataset')
+
+    # wrap the dataset
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset, num_replicas=args.world_size, rank=gpu, shuffle=train_cfg["shuffle"])
+    train_dataloader = DataLoader(train_dataset, shuffle=False, batch_size=train_cfg["batch_size"],
+                                  num_workers=train_cfg["num_workers"], pin_memory=True, sampler=train_sampler)
+
+    # wrap the model
+    model = nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+    # using apex
+    # model, optimizer = amp.initialize(model, optimizer, opt_level='O2', keep_batchnorm_fp32=True)
+    # model = DDP(model)
+
+    # loading pretrain model
+    if cfg['train_params']['model_num'] != 0:
+        model_path = log_dir + 'resnet_{}.pth'.format(cfg['train_params']['model_num'])
+        model.load_state_dict(torch.load(model_path, map_location={'cuda:%d' % 0: 'cuda:%d' % gpu})['model'])
+
+    # TRAIN LOOP
+    checkpoint = cfg['train_params']['checkpoint_every_n_steps']
+    for epoch in range(cfg['train_params']['max_num_epochs']):
+        print('Begin epoch {}'.format(epoch))
+        tr_it = iter(train_dataloader)
+        progress_bar = tqdm(range(len(train_dataloader)))
+        losses_train = []
+        for index in progress_bar:
+            try:
+                data = next(tr_it)
+            except StopIteration:
+                tr_it = iter(train_dataloader)
+                data = next(tr_it)
+            model.train()
+            torch.set_grad_enabled(True)
+            loss, _ = forward(data, model, gpu, criterion)
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses_train.append(loss.item())
+            progress_bar.set_description(f"loss: {loss.item()} loss(avg): {np.mean(losses_train)}")
+
+            if index % checkpoint == 0 and index != 0 and gpu == 0:
+                state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict()}
+                torch.save(state, log_dir + 'resnet_{}_{}.pth'.format(epoch, index))
+    print("Finish training")
 
 
-# ===== INIT DATASET
-train_cfg = cfg["train_data_loader"]
-rasterizer = build_rasterizer(cfg, dm)
-train_zarr = ChunkedDataset(dm.require(train_cfg["key"])).open()
-train_dataset = AgentDataset(cfg, train_zarr, rasterizer)
-train_dataloader = DataLoader(train_dataset, shuffle=train_cfg["shuffle"], batch_size=train_cfg["batch_size"], 
-                             num_workers=train_cfg["num_workers"])
-# num_workers=train_cfg["num_workers"]
-print(train_dataset)
-print(type(train_dataloader))
-
-
-# In[6]:
-
-
-# ==== INIT MODEL
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = build_model(cfg).to(device)
-if not cfg["train_params"]["load_the_state"]:
-    checkpoint_res_pretrain = torch.load(resnet_pre)
-    model.load_state_dict(checkpoint_res_pretrain['resnet_pretrained_model'])
-    print("resnet预训练加载成功")
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-criterion = nn.MSELoss(reduction="none")
-
-
-# # Training
-# 
-# note: if you're on MacOS and using `py_satellite` rasterizer, you may need to disable opencv multiprocessing by adding:
-# `cv2.setNumThreads(0)` before the following cell. This seems to only affect running in python notebook and it's caused by the `cv2.warpaffine` function
-
-# In[7]:
-if not cfg["train_params"]["load_the_state"]:
-    # ==== TRAIN LOOP
-    tr_it = iter(train_dataloader)
-    progress_bar = tqdm(range(cfg["train_params"]["max_num_steps"]))
-    losses_train = []
-    for _ in progress_bar:
-        try:
-            data = next(tr_it)
-        except StopIteration:
-            tr_it = iter(train_dataloader)
-            data = next(tr_it)
-        model.train()
-        torch.set_grad_enabled(True)
-        loss, _ = forward(data, model, device, criterion)
-
-        # Backward pass
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        losses_train.append(loss.item())
-        progress_bar.set_description(f"loss: {loss.item()} loss(avg): {np.mean(losses_train)}")
-        state = {'model':model.state_dict(), 'optimizer':optimizer.state_dict()}
-        torch.save(state, log_dir)
-    print("训练完成")
-
-else:
-    checkpoint = torch.load(log_dir)
+# Evaluation
+def evaluate():
+    model_path = log_dir + 'resnet_{}.pth'.format(cfg['train_params']['model_num'])
+    checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['model'])
     print("加载成功")
-# # Evaluation
-# 
-# Evaluation follows a slightly different protocol than training. When working with time series, we must be absolutely sure to avoid leaking the future in the data.
-# 
-# If we followed the same protocol of training, one could just read ahead in the `.zarr` and forge a perfect solution at run-time, even for a private test set.
-# 
-# As such, **the private test set for the competition has been "chopped" using the `chop_dataset` function**.
-# 
-# ## DISCLAIMER
-# **We're updating the dataset to support traffic lights. The code below has been designed to work with TLs, and it does not suppport the old interface. We expect the dataset to be online in the next few days** ( disclaimer added on 08/18/20)
+    # ===== GENERATE AND LOAD CHOPPED DATASET
+    eval_cfg = cfg['val_data_loader']
+    # 获取mask
+    mask_path = os.environ["L5KIT_DATA_FOLDER"] + eval_cfg["mask"]
+    # 导入mask
+    mask = np.load(mask_path)["arr_0"]
+    # 生成网格
+    rasterizer = build_rasterizer(cfg, dm)
+    # 获取包含训练数据集的实例，内部包含agents、frames和scenes
+    test_zarr = ChunkedDataset(dm.require(eval_cfg["key"])).open()
+    # 获取数据集内的特定内容
+    agent_dataset = AgentDataset(cfg, test_zarr, rasterizer)
+    # 加入mask，仅对mask指出的71122个agent进行运动预测，这些agent均只有前100帧而没有未来的50帧
+    mask_dataset = AgentDataset(cfg, test_zarr, rasterizer, agents_mask=mask)
 
-# In[8]:
+    eval_dataloader = DataLoader(mask_dataset, shuffle=eval_cfg["shuffle"], batch_size=eval_cfg["batch_size"],
+                                 num_workers=eval_cfg["num_workers"])
 
+    # EVAL LOOP
+    model.eval()
+    torch.set_grad_enabled(False)
 
-# ===== GENERATE AND LOAD CHOPPED DATASET
-num_frames_to_chop = 100
-eval_cfg = cfg["val_data_loader"]
-print(eval_cfg)
-print("中断")
-eval_base_path = create_chopped_dataset(dm.require(eval_cfg["key"]), cfg["raster_params"]["filter_agents_threshold"], 
-                              num_frames_to_chop, cfg["model_params"]["future_num_frames"], MIN_FUTURE_STEPS)
+    # store information for evaluation
+    future_coords_offsets_pd = []
+    timestamps = []
 
-
-# The result is that **each scene has been reduced to only 100 frames**, and **only valid agents in the 100th frame will be used to compute the metrics**. Because following frames in the scene have been chopped off, we can't just look ahead to get the future of those agents.
-# 
-# In this example, we simulate this pipeline by running `chop_dataset` on the validation set. The function stores:
-# - a new chopped `.zarr` dataset, in which each scene has only the first 100 frames;
-# - a numpy mask array where only valid agents in the 100th frame are True;
-# - a ground-truth file with the future coordinates of those agents;
-
-# In[9]:
-
-
-eval_zarr_path = str(Path(eval_base_path) / Path(dm.require(eval_cfg["key"])).name)
-eval_mask_path = str(Path(eval_base_path) / "mask.npz")
-eval_gt_path = str(Path(eval_base_path) / "gt.csv")
-
-eval_zarr = ChunkedDataset(eval_zarr_path).open()
-eval_mask = np.load(eval_mask_path)["arr_0"]
-# ===== INIT DATASET AND LOAD MASK
-eval_dataset = AgentDataset(cfg, eval_zarr, rasterizer, agents_mask=eval_mask)
-eval_dataloader = DataLoader(eval_dataset, shuffle=eval_cfg["shuffle"], batch_size=eval_cfg["batch_size"], 
-                             num_workers=eval_cfg["num_workers"])
-#num_workers=eval_cfg["num_workers"]
-print(eval_dataset)
+    agent_ids = []
+    progress_bar = tqdm(eval_dataloader)
+    for data in progress_bar:
+        _, ouputs = forward(data, model, device, criterion)
+        future_coords_offsets_pd.append(ouputs.cpu().numpy().copy())
+        timestamps.append(data["timestamp"].numpy().copy())
+        agent_ids.append(data["track_id"].numpy().copy())
 
 
-# Please note how `Num Frames==(Num Scenes)*num_frames_to_chop`. 
-# 
-# The remaining frames in the scene have been sucessfully chopped off from the data
-
-# In[10]:
-
-
-# ==== EVAL LOOP
-model.eval()
-torch.set_grad_enabled(False)
-
-# store information for evaluation
-future_coords_offsets_pd = []
-timestamps = []
-
-agent_ids = []
-progress_bar = tqdm(eval_dataloader)
-for data in progress_bar:
-    _, ouputs = forward(data, model, device, criterion)
-    future_coords_offsets_pd.append(ouputs.cpu().numpy().copy())
-    timestamps.append(data["timestamp"].numpy().copy())
-    agent_ids.append(data["track_id"].numpy().copy())
-    
+    # Save results
+    pred_path = "./pred.csv"
+    write_pred_csv(pred_path,
+                   timestamps=np.concatenate(timestamps),
+                   track_ids=np.concatenate(agent_ids),
+                   coords=np.concatenate(future_coords_offsets_pd),
+                  )
 
 
-# ### Save results
-# After the model has predicted trajectories for our evaluation set, we can save them in a `csv` file.
-# 
-# During the competition, only the `.zarr` and the mask will be provided for the private test set evaluation.
-# Your solution is expected to generate a csv file which will be compared to the ground truth one on a separate server
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
+    parser.add_argument('-g', '--gpus', default=4, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes')
+    args = parser.parse_args()
 
-# In[11]:
-
-
-pred_path = f"{gettempdir()}/pred.csv"
-
-write_pred_csv(pred_path,
-               timestamps=np.concatenate(timestamps),
-               track_ids=np.concatenate(agent_ids),
-               coords=np.concatenate(future_coords_offsets_pd),
-              )
+    args.world_size = args.gpus * args.nodes
+    if not cfg["train_params"]["load_the_state"]:
+        mp.spawn(train, nprocs=args.gpus, args=(args,))
+    else:
+        mp.spawn(evaluate, nprocs=args.gpus, args=(args,))
 
 
-# ### Perform Evaluation
-# Pleae note that our metric supports multi-modal predictions (i.e. multiple predictions for a single GT trajectory). In that case, you will need to provide a confidence for each prediction (confidences must all be between 0 and 1 and sum to 1).
-# 
-# In this simple example we don't generate multiple trajectories, so we won't pass any confidences vector. Internally, the metric computation will assume a single trajectory with confidence equal to 1
-
-# In[12]:
-
-
-metrics = compute_metrics_csv(eval_gt_path, pred_path, [neg_multi_log_likelihood, time_displace])
-for metric_name, metric_mean in metrics.items():
-    print(metric_name, metric_mean)
-
-
-# ### Visualise Results
-# We can also visualise some results from the ego (AV) point of view for those frames of interest (the 100th of each scene).
-# 
-# However, as we chopped off the future from the dataset **we must use the GT csv if we want to plot the future trajectories of the agents**
-# 
-
-# In[ ]:
-
-
-model.eval()
-torch.set_grad_enabled(False)
-
-# build a dict to retrieve future trajectories from GT
-gt_rows = {}
-for row in read_gt_csv(eval_gt_path):
-    gt_rows[row["track_id"] + row["timestamp"]] = row["coord"]
-
-eval_ego_dataset = EgoDataset(cfg, eval_dataset.dataset, rasterizer)
-
-for frame_number in range(99, len(eval_zarr.frames), 100):  # start from last frame of scene_0 and increase by 100
-    agent_indices = eval_dataset.get_frame_indices(frame_number) 
-    if not len(agent_indices):
-        continue
-
-    # get AV point-of-view frame
-    data_ego = eval_ego_dataset[frame_number]
-    im_ego = rasterizer.to_rgb(data_ego["image"].transpose(1, 2, 0))
-    center = np.asarray(cfg["raster_params"]["ego_center"]) * cfg["raster_params"]["raster_size"]
-    
-    predicted_positions = []
-    target_positions = []
-
-    for v_index in agent_indices:
-        data_agent = eval_dataset[v_index]
-
-        out_net = model(torch.from_numpy(data_agent["image"]).unsqueeze(0).to(device))
-        out_pos = out_net[0].reshape(-1, 2).detach().cpu().numpy()
-        # store absolute world coordinates
-        predicted_positions.append(out_pos + data_agent["centroid"][:2])
-        # retrieve target positions from the GT and store as absolute coordinates
-        track_id, timestamp = data_agent["track_id"], data_agent["timestamp"]
-        target_positions.append(gt_rows[str(track_id) + str(timestamp)] + data_agent["centroid"][:2])
-
-
-    # convert coordinates to AV point-of-view so we can draw them
-    predicted_positions = transform_points(np.concatenate(predicted_positions), data_ego["world_to_image"])
-    target_positions = transform_points(np.concatenate(target_positions), data_ego["world_to_image"])
-
-    yaws = np.zeros((len(predicted_positions), 1))
-    draw_trajectory(im_ego, predicted_positions, PREDICTED_POINTS_COLOR, yaws)
-    draw_trajectory(im_ego, target_positions, TARGET_POINTS_COLOR, yaws)
-
-    plt.imshow(im_ego[::-1])
-    plt.show()
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
